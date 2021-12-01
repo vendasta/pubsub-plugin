fe4476320bda28df446ccad8d3ea794b510edf9d
Jesse Redl <jredl@vendasta.com>
2021-12-01 16:25:26 -0600

    Revert code removal

--- index.ts
+++ index.ts
@@ -52,4 +52,58 @@ export async function exportEvents(events: PluginEvent[], { global, config }: Pl
     if (!global.pubSubClient) {
         throw new Error('No PubSub client initialized!')
     }
+    try {
+        const messages = events.map((fullEvent) => {
+            const { event, properties, $set, $set_once, distinct_id, team_id, site_url, now, sent_at, uuid, ...rest } =
+                fullEvent
+            const ip = properties?.['$ip'] || fullEvent.ip
+            const timestamp = fullEvent.timestamp || properties?.timestamp || now || sent_at
+            let ingestedProperties = properties
+            let elements = []
+
+            // only move prop to elements for the $autocapture action
+            if (event === '$autocapture' && properties?.['$elements']) {
+                const { $elements, ...props } = properties
+                ingestedProperties = props
+                elements = $elements
+            }
+
+            const message = {
+                event,
+                distinct_id,
+                team_id,
+                ip,
+                site_url,
+                timestamp,
+                uuid: uuid!,
+                properties: ingestedProperties || {},
+                elements: elements || [],
+                people_set: $set || {},
+                people_set_once: $set_once || {},
+            }
+            return Buffer.from(JSON.stringify(message))
+        })
+
+        const start = Date.now()
+        await Promise.all(
+            messages.map((dataBuf) =>
+                global.pubSubTopic.publish(dataBuf).then((messageId) => {
+                    return messageId
+                })
+            )
+        )
+        const end = Date.now() - start
+
+        console.log(
+            `Published ${events.length} ${events.length > 1 ? 'events' : 'event'} to ${config.topicId}. Took ${
+                end / 1000
+            } seconds.`
+        )
+    } catch (error) {
+        console.error(
+            `Error publishing ${events.length} ${events.length > 1 ? 'events' : 'event'} to ${config.topicId}: `,
+            error
+        )
+        throw new RetryError(`Error publishing to Pub/Sub! ${JSON.stringify(error.errors)}`)
+    }
 }
